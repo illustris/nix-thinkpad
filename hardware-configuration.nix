b4e7319ace12fc0916bb9bd62bdca700c0ea1cf8
illustris <rharikrishnan95@gmail.com>
2022-07-22 07:28:23 +0530

    enable nested virt

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -15,6 +15,9 @@
 		};
 		extraModprobeConfig = ''
 			options i915 enable_guc=2
+			options kvm_intel nested=1
+			options kvm_intel emulate_invalid_guest_state=0
+			options kvm ignore_msrs=1
 		'';
 		# rtl8814au USB WiFi adapter
 		extraModulePackages = [ config.boot.kernelPackages.rtl8814au ];
